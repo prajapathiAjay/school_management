e1d9fed2c0dffe9fe5de112e706c9d2427cf3b4b
prajapathiAjay <ajayworking6309@gmail.com>
2025-10-24 17:33:52 +0530

    updates

--- frontend/src/Layouts/Layout.jsx
+++ frontend/src/Layouts/Layout.jsx
@@ -1,40 +1,4 @@
-// import React, { useState } from "react";
-// import Header from "../common/Header";
-// import Sidebar from "../common/Sidebar";
 
-// const Layout = ({ children }) => {
-//   const [isCollapsed, setIsCollapsed] = useState(false);
-
-//   const toggleSidebar = () => {
-//     setIsCollapsed(!isCollapsed);
-//   };
-
-//   return (
-//     <div className="flex  bg-gray-50 min-h-screen">
-//       {/* Sidebar */}
-//       <Sidebar toggleSidebar={toggleSidebar} isCollapsed={isCollapsed} />
-
-//       <div className="flex-1 flex flex-col ">
-//         {/* Header */}
-//         <Header toggleSidebar={toggleSidebar} isCollapsed={isCollapsed} layoutType="full" />
-
-//         {/* Page Content */}
-//         {/* <main
-//           className={`flex-1 p-6 transition-all duration-300 ${
-//             isCollapsed ? "ml-20" : "ml-64"
-//           }`}
-//         > */}
-//          <main
-//           className={`flex p-6 transition-all duration-300 overflow-y-auto`}
-//         >
-//           {children}
-//         </main>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Layout;
 import React, { useState } from "react";
 import Header from "../common/Header";
 import Sidebar from "../common/Sidebar";
--- frontend/src/routes/RouterLinks.jsx
+++ frontend/src/routes/RouterLinks.jsx
@@ -28,7 +28,4 @@ export const privateRoutes = [
   },
 ];
 
-// export const publicRoutes = [
-//   {icon:"home",path:"/",component:<Navigate to={allRoutes.login} replace />},
-//   {icon:"login",path:allRoutes.login,component:<Login/>}
-// ];
+
